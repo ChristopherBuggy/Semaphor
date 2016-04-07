7d5bd5c859d183cef56de23c1d5bd5829e7aab9a
christopherbuggy <christopherbuggy94@gmail.com>
2016-04-07 11:28:35 +0100

    Inital

---
+++ Semaphor with boost/MonitorLab/Monitor.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+#include "Monitor.h"
+
+Monitor::Monitor()
+{
+	front = 0;
+	rear = 0;
+	count = 0;
+	not_full = true;
+	not_empty = false;
+
+	newMessage = false;
+}
+
+Monitor::~Monitor()
+{
+}
+
+void Monitor::Deposit(std::string data)
+{
+	while (count == 100)//n
+	{
+		while (not_full == false) {}
+	}
+	buf[rear] = data;
+	rear = (rear + 1) % 10;
+	count++;
+	not_empty = false;
+	newMessage = true;
+}
+
+void Monitor::Fetch(std::string &result)
+{
+	while (count == 0)
+	{
+		while (not_empty == false) {}
+	}
+	result = buf[front];
+	front = (front + 1) % 10;
+	count--;
+	not_full = false;
+	newMessage = false;
+}
+
+bool Monitor::Empty()
+{
+	if (not_empty == true)
+		return false;
+	else return true;
+}
+
+bool Monitor::NewMsgAdded()
+{
+	return newMessage;
+}
---
+++ Semaphor with boost/MonitorLab/stdafx.h
@@ -0,0 +1,32 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#ifndef STDAFX_H
+#define STDAFX_H
+
+//#Defines
+#define SCREENWIDTH 600
+#define SCREENHEIGHT 400
+
+
+#include "targetver.h"
+
+//SFML includes
+#include "SFML\Graphics.hpp"
+#include "SFML\OpenGL.hpp"
+#include "SFML\Window\Keyboard.hpp"
+#include "SFML/Audio.hpp"
+
+//Standard includes
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>     /* srand, rand */
+
+//project headers
+#include "Monitor.h"
+// TODO: reference additional headers your program requires here
+#endif
